74fff683bd1f9862ce7f8c33e67594190c840559
Ronny Gutierrez <ronny.icne@gmail.com>
2018-06-04 11:53:07 -0700

    11:52am

--- server.js
+++ server.js
@@ -3,14 +3,18 @@ const serveStatic = require("serve-static");
 
 const path = require("path");
 
-const app = express();
 
+//create the express app
+const app = express();
+ 
+//create middleware to handle the serving of the app
 app.use("/", serveStatic(path.join(__dirname, "/public")));
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/public/index.html");
 });
 
+//create default port to serve the app
 const port = process.env.PORT || 5000;
 
 app.listen(port);
