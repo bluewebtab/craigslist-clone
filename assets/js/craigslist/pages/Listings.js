e98811923095791132407b79ea894f787e889171
Ronny Gutierrez <ronny.icne@gmail.com>
2018-06-05 01:30:21 -0700

    1:30am

--- assets/js/craigslist/pages/Listings.js
+++ assets/js/craigslist/pages/Listings.js
@@ -16,7 +16,76 @@ export default class Listings extends Component {
     return (
       <div className = "listings">
         <div className = "container">
-        this listing is {match.params.listings} 
+        <section className = "filter">
+          <div className="form-group price">
+            <label>Price</label>
+            <select name = "min-price" className="min-price">
+              <option value = "0">0</option>
+
+            </select>
+            <select name = "max-price" className="max-price">
+              <option value = "1000">1000</option>
+
+            </select>
+
+          </div>
+          <div className="form-group make">
+            <label>Make</label>
+            <select name = "make" className="make">
+              <option value = "bmw">bmw</option>
+
+            </select>
+            
+          </div>
+          <div className="form-group model">
+            <label>Model</label>
+            <select name = "model" className="model">
+              <option value = "bmw">bmw</option>
+
+            </select>
+            
+          </div>
+          <div className="form-group button">
+           <div className="primary-btn">Update</div>
+           <div className="reset-btn">Reset</div>
+
+          </div>
+        </section>
+        <section className="list-view">
+          <section className="change-view">
+          <div className="form-group view-dropdown">
+            <select name = "model" className="select-view">
+              <option value = "gallery">Gallery view</option>
+              <option value = "list">List view</option>
+              <option value = "thumb">List view</option>
+
+
+            </select>
+            
+          </div>
+          <div className="form-group sort-dropdown">
+            <select name = "model" className="sort-dropdown">
+              <option value = "gallery">Newest</option>
+              </select>
+            
+          </div>
+          </section>
+          <section className="all-items">
+            <div className="item">
+              <div className = "image">
+              <div className="price">
+                $9000
+              </div>
+              image
+              </div>
+              <div className="details">
+                <h5>Title</h5>
+                <h6>City</h6>
+              </div>
+            </div>
+          
+          </section>
+        </section>
         </div>      
       </div>
     )
--- public/js/components/craigslist.js
+++ public/js/components/craigslist.js
@@ -598,8 +598,163 @@ var Listings = function (_Component) {
         _react2.default.createElement(
           'div',
           { className: 'container' },
-          'this listing is ',
-          match.params.listings
+          _react2.default.createElement(
+            'section',
+            { className: 'filter' },
+            _react2.default.createElement(
+              'div',
+              { className: 'form-group price' },
+              _react2.default.createElement(
+                'label',
+                null,
+                'Price'
+              ),
+              _react2.default.createElement(
+                'select',
+                { name: 'min-price', className: 'min-price' },
+                _react2.default.createElement(
+                  'option',
+                  { value: '0' },
+                  '0'
+                )
+              ),
+              _react2.default.createElement(
+                'select',
+                { name: 'max-price', className: 'max-price' },
+                _react2.default.createElement(
+                  'option',
+                  { value: '1000' },
+                  '1000'
+                )
+              )
+            ),
+            _react2.default.createElement(
+              'div',
+              { className: 'form-group make' },
+              _react2.default.createElement(
+                'label',
+                null,
+                'Make'
+              ),
+              _react2.default.createElement(
+                'select',
+                { name: 'make', className: 'make' },
+                _react2.default.createElement(
+                  'option',
+                  { value: 'bmw' },
+                  'bmw'
+                )
+              )
+            ),
+            _react2.default.createElement(
+              'div',
+              { className: 'form-group model' },
+              _react2.default.createElement(
+                'label',
+                null,
+                'Model'
+              ),
+              _react2.default.createElement(
+                'select',
+                { name: 'model', className: 'model' },
+                _react2.default.createElement(
+                  'option',
+                  { value: 'bmw' },
+                  'bmw'
+                )
+              )
+            ),
+            _react2.default.createElement(
+              'div',
+              { className: 'form-group button' },
+              _react2.default.createElement(
+                'div',
+                { className: 'primary-btn' },
+                'Update'
+              ),
+              _react2.default.createElement(
+                'div',
+                { className: 'reset-btn' },
+                'Reset'
+              )
+            )
+          ),
+          _react2.default.createElement(
+            'section',
+            { className: 'list-view' },
+            _react2.default.createElement(
+              'section',
+              { className: 'change-view' },
+              _react2.default.createElement(
+                'div',
+                { className: 'form-group view-dropdown' },
+                _react2.default.createElement(
+                  'select',
+                  { name: 'model', className: 'select-view' },
+                  _react2.default.createElement(
+                    'option',
+                    { value: 'gallery' },
+                    'Gallery view'
+                  ),
+                  _react2.default.createElement(
+                    'option',
+                    { value: 'list' },
+                    'List view'
+                  ),
+                  _react2.default.createElement(
+                    'option',
+                    { value: 'thumb' },
+                    'List view'
+                  )
+                )
+              ),
+              _react2.default.createElement(
+                'div',
+                { className: 'form-group sort-dropdown' },
+                _react2.default.createElement(
+                  'select',
+                  { name: 'model', className: 'sort-dropdown' },
+                  _react2.default.createElement(
+                    'option',
+                    { value: 'gallery' },
+                    'Newest'
+                  )
+                )
+              )
+            ),
+            _react2.default.createElement(
+              'section',
+              { className: 'all-items' },
+              _react2.default.createElement(
+                'div',
+                { className: 'item' },
+                _react2.default.createElement(
+                  'div',
+                  { className: 'image' },
+                  _react2.default.createElement(
+                    'div',
+                    { className: 'price' },
+                    '$9000'
+                  ),
+                  'image'
+                ),
+                _react2.default.createElement(
+                  'div',
+                  { className: 'details' },
+                  _react2.default.createElement(
+                    'h5',
+                    null,
+                    'Title'
+                  ),
+                  _react2.default.createElement(
+                    'h6',
+                    null,
+                    'City'
+                  )
+                )
+              )
+            )
+          )
         )
       );
     }
