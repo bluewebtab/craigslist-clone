2704d188f3767bceda3acbfb4fad10e89583e7b8
Ronny Gutierrez <ronny.icne@gmail.com>
2018-06-09 16:17:03 -0700

    4:16pm

--- data/categories.js
+++ data/categories.js
@@ -30,8 +30,243 @@ module.exports = [
   name: 'groups',
   slug: 'groups'
 },
-
-
+{
+  name: 'local news',
+  slug: 'local-news'
+}
     ]
   },
+
+
+{
+  title: "housing",
+  listings: [
+    {
+       name: 'apts/housing',
+       slug: 'apts-or-housing'
+    },
+    {
+      name: 'housing swap',
+      slug: 'housing-swap'
+   },
+   {
+    name: 'housing wanted',
+    slug: 'housing-wanted'
+ },
+ {
+  name: 'office / commercial',
+  slug: 'office-or-commercial'
+},
+{
+name: 'parking / storage',
+slug: 'parking-storage'
+},
+{
+name: 'real estate for sale',
+slug: 'real-estate-for-sale'
+},
+{
+name: 'rooms / shared',
+slug: 'rooms-or-shared'
+},
+{
+name: 'rooms wanted',
+slug: 'rooms-wanted'
+}
+  ]
+},
+
+{
+  title: "jobs",
+  listings: [
+    {
+       name: 'accounting+finance',
+       slug: 'accounting-and-finance'
+    },
+    {
+      name: 'admin / finance',
+      slug: 'admin-or-finance'
+   },
+   {
+    name: 'arch / engineering',
+    slug: 'arch-or-engineering'
+ },
+ {
+  name: 'art / media / design',
+  slug: 'art-media-design'
+},
+{
+name: 'biotech / science',
+slug: 'biotech-science'
+},
+{
+name: 'business / mgmt',
+slug: 'business-mgmt'
+},
+{
+name: 'customer service',
+slug: 'customer-service'
+},
+{
+name: 'education',
+slug: 'education'
+}
+  ]
+},
+
+{
+  title: "for-sale",
+  listings: [
+    {
+       name: 'antiques',
+       slug: 'antiques'
+    },
+    {
+      name: 'appliances',
+      slug: 'appliances'
+   },
+   {
+    name: 'arts+crafts',
+    slug: 'arts-and-crafts'
+ },
+ {
+  name: 'auto parts',
+  slug: 'auto-parts'
+},
+{
+name: 'bikes',
+slug: 'bikes'
+},
+{
+name: 'boats',
+slug: 'boats'
+},
+{
+name: 'books',
+slug: 'books'
+},
+{
+name: 'cars+trucks',
+slug: 'cars-and-trucks'
+}
+  ]
+},
+
+{
+  title: "discussion-forums",
+  listings: [
+    {
+       name:'celebs',
+       slug: 'celebs'
+    },
+    {
+      name: 'comp',
+      slug: 'comp'
+   },
+   {
+    name: 'crafts',
+    slug: 'crafts'
+ },
+ {
+  name: 'hats',
+  slug: 'hats'
+},
+{
+name: 'history',
+slug: 'history'
+},
+{
+name: 'housing',
+slug: 'housing'
+},
+{
+name: 'jobs',
+slug: 'jobs'
+},
+{
+name: 'psych',
+slug: 'psych'
+}
+  ]
+},
+
+{
+  title: "services",
+  listings: [
+    {
+       name: 'automotive',
+       slug: 'automotive'
+    },
+    {
+      name: 'beauty',
+      slug: 'beauty'
+   },
+   {
+    name: 'cell / mobile',
+    slug: 'cell-and-mobile'
+ },
+ {
+  name: 'computer',
+  slug: 'computer'
+},
+{
+name: 'creative',
+slug: 'creative'
+},
+{
+name: 'cycle',
+slug: 'cycle'
+},
+{
+name: 'event',
+slug: 'event'
+},
+{
+name: 'lessons',
+slug: 'lessons'
+}
+  ]
+},
+
+{
+  title: "gigs",
+  listings: [
+    {
+       name: 'computer',
+       slug: 'computer'
+    },
+    {
+      name: 'creative',
+      slug: 'creative'
+   },
+   {
+    name: 'crew',
+    slug: 'crew'
+ },
+ {
+  name: 'domestic',
+  slug: 'domestic'
+},
+{
+name: 'events',
+slug: 'events'
+},
+{
+name: 'labor',
+slug: 'labor'
+},
+{
+name: 'talent',
+slug: 'talent'
+},
+{
+name: 'writing',
+slug: 'writing'
+}
+  ]
+}
+
+
+
+
 ]
